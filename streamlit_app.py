56c1f19c67f3c8f01ecdc9207d714617524b228c
khanh <khanhduc@gmail.com>
2021-01-08 20:16:01 +0100

    ii

--- streamlit_app.py
+++ streamlit_app.py
@@ -470,7 +470,7 @@ def overview():
             thickness=1,
             size=20 * 0.99,  # controls width of tick.
         ).encode(
-            x=alt.X('Operator:N',axis=alt.Axis(title='Operators', labels=False)),
+            x=alt.X('Operator:N',axis=alt.Axis(title='Operators', labelAngle=-45)),
             opacity=alt.condition(pts, alt.value(1.0), alt.value(0.2)),
             y=alt.Y('sum(Remaining_OE):Q',title='MSM³OE', scale = alt.Scale(type='log'))
         ).transform_filter(
