b1c13d5802fc34e8d6baca9725756697f4c571a2
khanh <khanhduc@gmail.com>
2021-01-13 20:26:38 +0100

    ccc

--- streamlit_app.py
+++ streamlit_app.py
@@ -650,8 +650,8 @@ def overview():
 #                                            'color': "darkgreen" if x['properties']['Dctype']=='OIL' else ( "darkred" if x['properties']['Dctype']=='GAS' else ("darkorange" if x['properties']['Dctype']=='OIL/GAS' else "darkblue"))}
 
 # add marker
-                folium.LocateMarker([lat, lon],radius=3,fill=True,color=col, tooltip=fields).add_to(m)
-
+#                folium.CircleMarker([lat, lon],radius=3,fill=True,color=col, tooltip=fields).add_to(m)
+                folium.Marker([lat, lon],icon(color=col,icon_color=col,icon='screenshot'), tooltip=fields).add_to(m)
         # call to render Folium map in Streamlit
                 minimap = MiniMap(toggle_display=True,position="topright",tile_layer="cartodbpositron",zoom_level_offset=-6,width=120, height=150)
                 minimap.add_to(m)
