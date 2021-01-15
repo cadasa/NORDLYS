9150a57b0207c1534e8927ddd217d6c7bdc42c3d
khanh <khanhduc@gmail.com>
2021-01-15 15:53:27 +0100

    sssss

--- streamlit_app.py
+++ streamlit_app.py
@@ -454,8 +454,8 @@ def overview():
 
         # Top panel is scatter plot of temperature vs time
         points = alt.Chart(df_dsc_fld).transform_filter("datum.Year >= 1967").mark_point().encode(
-            alt.X('Year:N',title='Discovery Year', labelAngle=-20),
-            alt.Y('Recoverable OE:Q',title='Recoverable Reserves in MSM³OE'),
+            alt.X('Year:N',title='Discovery Year'),
+            alt.Y('Recoverable OE:Q',title='Recoverable Reserves in MSM³OE', scale = alt.Scale(type='log')),
             tooltip=['Discovery Year','Operator:N','Recoverable OE:Q','Remaining OE:Q'],
             color=alt.condition(brush, color, alt.value('lightgray')),
             size=alt.Size('Remaining OE:Q', legend=alt.Legend(title='Remaining OE in MSM³OE',orient='bottom'))
