57180f5996acefd2ec117154826385674c5fd011
khanh <khanhduc@gmail.com>
2021-01-19 14:58:41 +0100

    ccc

--- streamlit_app.py
+++ streamlit_app.py
@@ -208,7 +208,11 @@ def field():
     prod_fields.loc[:,'prfPrdCondensateNetMillSm3']=prod_fields.loc[:,'prfPrdCondensateNetMillSm3'].astype(float)
     sum_cond = prod_fields.groupby('prfYear')['prfPrdCondensateNetMillSm3'].transform(lambda x: x.sum())
     prod_fields['Sum_Condensate'] = sum_cond
-    prod_year_sum = prod_fields.drop_duplicates(subset = ['prfYear'])[['prfYear','Year','Sum_Oil','Sum_Gas','Sum_NGL','Sum_Condensate']]
+    Sum_Remaining_Reserves = prod_fields.groupby('prfYear')['Remaining_Reserves'].transform(lambda x: x.sum())
+    prod_fields['Sum_Remaining_Reserves'] = Sum_Remaining_Reserves
+    CumSum_Production = prod_fields.groupby('prfYear')['Cum_Prod'].transform(lambda x: x.sum())
+    prod_fields['CumSum_Production'] = CumSum_Production
+    prod_year_sum = prod_fields.drop_duplicates(subset = ['prfYear'])[['prfYear','Year','Sum_Oil','Sum_Gas','Sum_NGL','Sum_Condensate','Sum_Remaining_Reserves','CumSum_Production']]
 #    st.dataframe(prod_year_sum)
     prod_fieldnames = prod_fields.drop_duplicates(subset = ['fldName'])['fldName'].to_list()
     all = ['ALL']
@@ -235,17 +239,15 @@ def field():
             width=585, height=350
         ).interactive()
 
-        c1b = alt.Chart(prod_fields).mark_point(color='black',strokeWidth=1,shape='triangle-down',yOffset=-3).encode(
+        c1b = alt.Chart(prod_year_sum).mark_point(color='black',strokeWidth=1,shape='triangle-down',yOffset=-3).encode(
                 alt.Y('Sum_Remaining_Reserves:Q',
                     axis=alt.Axis(title='Reserves in Millions Standard m³ Oil Equivalent')
                 ),
                 alt.X('year(Year):T',
                     axis=alt.Axis(format='%Y',labelAngle=0, title='Producing Year')),
                 tooltip=['year(Year):T', 'Sum_Production:Q', 'CumSum_Production:Q', 'Sum_Remaining_Reserves:Q'],
-            ).transform_aggregate(
-                Sum_Remaining_Reserves='sum(Remaining_Reserves)', Sum_Production='sum(Production)',
-                CumSum_Production='sum(Cum_Prod)',
-                groupby=["Year"]
+            ).transform_calculate(
+                Sum_Production='Datum.Sum_Gas'+'Datum.Sum_NGL'+'Datum.Sum_Oil'+'Datum.Sum_Condensate'
             ).add_selection(hover2).transform_filter(click)
 
         c1c = alt.Chart(prod_year_sum).mark_bar(size=10).encode(
