871d5032b460e27dd186a5333a6d31e4b2a87cb5
khanh <khanhduc@gmail.com>
2021-02-03 00:18:27 +0100

    slo

--- streamlit_app.py
+++ streamlit_app.py
@@ -252,13 +252,22 @@ def field():
             width=585, height=320
         ).interactive()
 
+        # Transparent selectors across the chart. This is what tells us
+        # the x-value of the cursor
+        selectors = alt.Chart(prod_fields).mark_point().encode(
+            alt.X('year(Year):T',
+                axis=alt.Axis(labelFlush=False,format='%Y',labelAngle=0, title='Producing Year')),
+            opacity=alt.value(0),
+        ).add_selection(
+            nearest
+        )
         # Draw a rule at the location of the selection
         rules = alt.Chart(prod_fields).mark_rule(color='gray', strokeDash=[5,5]).encode(
             alt.X('year(Year):T',
                 axis=alt.Axis(labelFlush=False,format='%Y',labelAngle=0, title='Producing Year')),
         ).transform_filter(
             nearest
-        ).add_selection(nearest)
+        )
 
         c1b = alt.Chart(prod_year_sum).mark_point(size=35,clip=False,align='left',color='black',strokeWidth=1.5,shape='triangle-down',yOffset=-3).encode(
                 alt.Y('Sum_Remaining_Reserves:Q',
@@ -308,7 +317,7 @@ def field():
             opacity=alt.condition(hover|click, alt.value(1.0), alt.value(0.2))
             ).properties(title="TOTAL PRODUCTION",width=150,height=320)
 
-        c = (c1c&((c1a+c1b+rules)|c2)).resolve_scale(color='independent')
+        c = (c1c&((c1a+c1b+rules+selectors)|c2)).resolve_scale(color='independent')
 #        c1 = alt.hconcat((c1a+c1b),c2)
 #        c = alt.vconcat(c1c,c1).resolve_scale(color='independent')
         # Turn of the dots menu
